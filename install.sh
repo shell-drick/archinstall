593ce637e7cc2983ddb322209c65725cda18492a
Andrew Sheldrick <ajsheld12@gmail.com>
2022-12-20 01:52:15 -0500

    oops

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/shell-drick/archinstall/master/splinter.json -o /root/config.json
-curl https://raw.githubusercontent.com/shell-drick/archinstall/master/creds.json -o /root/creds.json
+curl https://raw.githubusercontent.com/shell-drick/archinstall/master/users.json -o /root/creds.json
 
 archinstall --config /root/config.json --creds /root/creds.json
